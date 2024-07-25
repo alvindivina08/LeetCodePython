2cdd9a4add49c1ef309d089598b024a60047cb50
alvindivina08 <alvindivina08@gmail.com>
2024-07-25 17:06:59 -0500

    binary search

--- Medium/BinarySearch/search_2d_matrix.py
+++ Medium/BinarySearch/search_2d_matrix.py
@@ -33,3 +33,6 @@ class Solution:
             else:
                 return True
         return False
+    
+matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+matrix2 = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
