0baa261ef2b5467a5938b09b4c4ee56b8e5f27a9
alvindivina08 <alvindivina08@gmail.com>
2024-07-25 17:26:25 -0500

    add comments

--- Easy/BinarySearch/binary_search.py
+++ Easy/BinarySearch/binary_search.py
@@ -1,17 +1,20 @@
-from typing import List
-
-
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         l, r = 0, len(nums) - 1
 
+        # while left is less than right
         while l < r:
+            # find the middle index by adding l + r divide by two
             m = (l + r) // 2
-
+            # then find
+            # if nums middle is less than target
             if nums[m] < target:
                 r = m + 1
+            # if nums middle is greater than target
             elif nums[m] > target:
                 l = m - 1
+            # if none of the above is true
+            # it means we found the index of the target
             else:
                 return m
                 
